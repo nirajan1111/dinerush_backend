e3eec3146bee98517031b04de1fd0d0ac250e92e
nirajan1111 <nirajansah1111@gmail.com>
2025-05-22 18:19:07 +0545

    configured for vercel

---
+++ api/index.js
@@ -0,0 +1,9 @@
+import app from "./../index.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    });
+export default app;
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import {
   getPlaces,
   getPlaceSuggestions,
@@ -9,10 +8,10 @@ import {
   getAllRestaurants,
 } from "./controllers/places.controllers.js";
 
-dotenv.config();
+
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+
 
 app.use(cors());
 app.use(express.json());
@@ -28,6 +27,4 @@ app.get("/api/place-details", getPlaceDetails);
 app.get("/api/geocode", geocodeAddress);
 app.post("/api/restaurants", getAllRestaurants);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+export default app;
